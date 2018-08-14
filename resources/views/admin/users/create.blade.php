9aa362002b11c8796bab4b072a5682f6ae5adb89
Marek <mareknguyen2307@gmail.com>
2018-08-14 13:43:24 +0200

    users page working without photos

---
+++ resources/views/admin/users/create.blade.php
@@ -0,0 +1,44 @@
+@extends('layouts.admin')
+@section('content')
+<h1>Create Users</h1>
+<form method="POST" action="{{route('admin.users.store')}}" enctype="multipart/form-data">
+    <div class="form-group">
+        <label name="name">Name:</label>
+        <input name="name" class="form-control" />
+        <input type="hidden" name="_token" value="{{ csrf_token() }}">
+    </div>
+    <div class="form-group">
+        <label name="email">Email:</label>
+        <input name="email" class="form-control" />
+    </div>
+    <div class="form-group">
+        <label name="role_id">Role:</label>
+        <select name="role_id" class="form-control">
+            <option value="" selected disabled hidden>Choose Role</option>
+            @foreach($roles as $role)
+            <option value="{{$role->id}}">{{$role->name}}</option>
+                @endforeach
+        </select>
+    </div>
+    <div class="form-group">
+        <label name="status">Status:</label>
+        <select class="form-control" name="is_active">
+            <option value="0">Not Active</option>
+            <option value="1">Active</option>
+        </select>
+    </div>
+    <div class="form-group">
+        <label name="file">File:</label>
+        <input type="file" name="file"/>
+    </div>
+    <div class="form-group">
+        <label name="password">Password:</label>
+        <input name="password" type="password" class="form-control" />
+    </div>
+
+    <div class="form-group">
+        <button class="btn btn-primary" type="submit">Create User</button>
+    </div>
+</form>
+    @include('includes.form-error')
+@endsection
